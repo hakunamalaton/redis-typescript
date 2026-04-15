541fa2b5111100ab0291b6cec4381cbecd0c054d
Daniel Lam <toan.lt@holistics.io>
2026-04-15 14:47:22 +0700

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,16 @@
 import * as net from "net";
 import { parse } from "./parser";
+import { parseArgs } from "util";
+
+const { values } = parseArgs({
+  args: Bun.argv.slice(2),
+  options: {
+    port: {
+      type: "string",
+      default: "6379",
+    },
+  },
+});
 
 const server: net.Server = net.createServer((connection: net.Socket) => {
   connection.on("data", async (data: Buffer) => {
@@ -10,4 +21,4 @@ const server: net.Server = net.createServer((connection: net.Socket) => {
   });
 });
 
-server.listen(6379, "127.0.0.1");
+server.listen(Number(values.port), "127.0.0.1");
